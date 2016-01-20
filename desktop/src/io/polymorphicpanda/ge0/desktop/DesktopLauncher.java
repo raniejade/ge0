5cb4c1aa6787d605ffc610a65a1264b48e72baec
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2016-01-20 19:23:09 +0800

    Vanilla libgdx setup

---
+++ desktop/src/io/polymorphicpanda/ge0/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package io.polymorphicpanda.ge0.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import io.polymorphicpanda.ge0.GeOEditor;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GeOEditor(), config);
+	}
+}
